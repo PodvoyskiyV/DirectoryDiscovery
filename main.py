97b04b5d652fe2fc5fd977e97c7c3dc61d4d2215
Vadim Podvoyskiy <vadim.podvoyskiy@gmail.com>
2022-08-04 22:17:41 +0500

    target URL and file name added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# to start main.py wordlists {Target URL} {Wordlist} level of recursive
+# to start main.py wordlists {Target URL} {Wordlist} level of recursion, switches to choose wordlist http://example.com
 
 import pyfiglet
 import requests
@@ -7,9 +7,44 @@ import sys
 banner = pyfiglet.figlet_format('DirDiscovery')
 print(banner)
 
+
+def chosen_file(number):
+    switcher = {
+        1: 'best15.txt',
+        2: 'best110.txt',
+        3: 'best1050.txt',
+        4: 'big.txt',
+        5: 'small.txt',
+        6: 'common.txt',
+        7: 'indexes.txt',
+        8: 'names.txt'
+    }
+    return switcher.get(number)
+
+
 try:
-    target_url = input('[*] Enter Target URL: ')
-    file_name = input('[*] Enter Name Of The File Containing Directories: ')
+    target_url = input('[*] Enter Target URL like http(s)://example.com: ')
+    while True:
+        if target_url[:8] == 'https://' or target_url[:7] == 'http://':
+            break
+        else:
+            print('Target URL should be like http(s)://example.com')
+            target_url = input('[*] Enter Target URL: ')
+
+    print('You have some files to choose:')
+    print('best15 [1], best110 [2], best1050 [3], big [4], small [5], common [6], indexes [7], names [8]')
+    print('View all this files you can at https://github.com/PodvoyskiyV/DirectoryDiscovery/tree/master/wordlists')
+    file_name = input('[*] Enter a number corresponding to the file name: ')
+    while True:
+        if file_name in ['1', '2', '3', '4', '5', '6', '7', '8']:
+            file_name = chosen_file(int(file_name))
+            break
+        else:
+            print('You have some files to choose:')
+            print('best15 [1], best110 [2], best1050 [3], big [4], small [5], common [6], indexes [7], names [8]')
+            file_name = input('[*] Enter a number corresponding to the file name: ')
+
+    level_of_recursion = input('[*] Enter Level Of Recursion: ')
 
 
     def request(url):
