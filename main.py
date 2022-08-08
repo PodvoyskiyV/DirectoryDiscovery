caa3df9c8d3d1aeb35f85812e360f9aea3c79d5b
Vadim Podvoyskiy <vadim.podvoyskiy@gmail.com>
2022-08-08 17:24:38 +0500

    Added colors at user interface. Added availability to choose recursion lvl of directories scanning

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-# to start main.py wordlists {Target URL} {Wordlist} level of recursion, switches to choose wordlist http://example.com
-
 import pyfiglet
 import requests
 import sys
+import datetime
+from termcolor import colored
 
 banner = pyfiglet.figlet_format('DirDiscovery')
-print(banner)
+print(colored(banner, 'green'))
 
 
 def chosen_file(number):
@@ -22,56 +22,105 @@ def chosen_file(number):
     return switcher.get(number)
 
 
+def request(url):
+    try:
+        return requests.get(url, timeout=None).status_code
+    except requests.exceptions.ConnectionError:
+        pass
+
+
+def directory_discovery(current_lvl, target):
+    file = open(f'wordlists/{file_name}', 'r')
+    for line in file:
+        directory = line.strip()
+        full_url = target + '/' + directory
+        response = request(full_url)
+        if response != 404:
+            result.write(f'{full_url} {response} \n')
+            if response // 100 == 1:
+                print('Discovered Directory At This Path: ' + colored(full_url, 'blue'), colored(response, 'blue'))
+            elif response // 100 == 2:
+                print('Discovered Directory At This Path: ' + colored(full_url, 'blue'), colored(response, 'green'))
+            elif response // 100 == 3:
+                print('Discovered Directory At This Path: ' + colored(full_url, 'blue'), colored(response, 'yellow'))
+            else:
+                print('Discovered Directory At This Path: ' + colored(full_url, 'blue'), colored(response, 'red'))
+            if current_lvl + 1 <= lvl_of_recursion:
+                directory_discovery(current_lvl + 1, full_url)
+    file.close()
+
+
 try:
     target_url = input('[*] Enter Target URL like http(s)://example.com: ')
     while True:
         if target_url[:8] == 'https://' or target_url[:7] == 'http://':
-            break
+            if request(target_url) == 200:
+                break
+            else:
+                print(colored('Response of your URL is not 200', 'red'))
+                print('Check your URL')
+                target_url = input('[*] Enter Target URL: ')
         else:
-            print('Target URL should be like http(s)://example.com')
+            print(colored('Target URL is incorrect.', 'red'))
+            print('Target URL should be like', colored('http(s)://example.com', 'green'))
             target_url = input('[*] Enter Target URL: ')
 
-    print('You have some files to choose:')
+    git = 'https://github.com/PodvoyskiyV/DirectoryDiscovery/tree/master/wordlists'
+    print('\n You have some files to choose:')
     print('best15 [1], best110 [2], best1050 [3], big [4], small [5], common [6], indexes [7], names [8]')
-    print('View all this files you can at https://github.com/PodvoyskiyV/DirectoryDiscovery/tree/master/wordlists')
+    print('View all this files you can at', colored(git, 'blue'))
     file_name = input('[*] Enter a number corresponding to the file name: ')
     while True:
         if file_name in ['1', '2', '3', '4', '5', '6', '7', '8']:
             file_name = chosen_file(int(file_name))
             break
         else:
-            print("Typed that doesn't match any of the filenames")
+            print(colored("Typed that doesn't match any of the filenames", 'red'))
             print('You have some files to choose:')
             print('best15 [1], best110 [2], best1050 [3], big [4], small [5], common [6], indexes [7], names [8]')
             file_name = input('[*] Enter a number corresponding to the file name from 1 to 8: ')
 
-    print('Level of recursion can help you to make more deep search of directories')
-    print('Example: http://example.com/lvl1/lvl2/lvl3/lvl4/lvl5')
-    level_of_recursion = input('[*] Enter Level Of Recursion from 1 to 5: ')
+    print('\n Level of recursion can help you to make more deep search of directories')
+    print('Example:', colored('http://example.com/lvl1/lvl2/lvl3/lvl4/lvl5', 'blue'))
+    lvl_of_recursion = input('[*] Enter Level Of Recursion from 1 to 9: ')
     while True:
-        if level_of_recursion in ['1', '2', '3', '4', '5']:
-            level_of_recursion = int(level_of_recursion)
+        if lvl_of_recursion in ['1', '2', '3', '4', '5', '6', '7', '8', '9']:
+            lvl_of_recursion = int(lvl_of_recursion)
             break
         else:
-            print('Level of Recursion is incorrect')
-            level_of_recursion = input('[*] Enter Level Of Recursion from 1 to 5: ')
+            print(colored('Level of Recursion is incorrect', 'red'))
+            lvl_of_recursion = input('[*] Enter Level Of Recursion from 1 to 9: ')
 
+    start = datetime.datetime.now()
+    print("\n" + "-" * 50)
+    print("Scanning Target: " + target_url)
+    print("Level of Recursion:", lvl_of_recursion)
+    print("Wordlist: " + file_name)
+    print("Scanning started at: " + str(start))
+    print("-" * 50 + "\n")
 
-    def request(url):
-        try:
-            return requests.get(url)
-        except requests.exceptions.ConnectionError:
-            pass
+    result = open('result.txt', 'w+')
+    result.write(pyfiglet.figlet_format('Directories'))
+    result.write('\n')
+    result.close()
+    result = open('result.txt', 'a+')
 
+    directory_discovery(1, target_url)
 
-    file = open(f'wordlists/{file_name}', 'r')
-    for line in file:
-        directory = line.strip()
-        full_url = target_url + '/' + directory
-        response = request(full_url)
-        if response:
-            print('[*] Discovered Directory At This Path: ' + full_url)
+    result.close()
+
+    end = datetime.datetime.now()
+    print("\n" + "-" * 50)
+    print("Scanning ended at: " + str(end))
+    print("Scanning duration is: " + str(end-start))
+    print("-" * 50 + "\n")
+
 
 except KeyboardInterrupt:
-    print("\n Program finished !!!!")
+    try:
+        result.close()
+    except NameError:
+        pass
+
+    print("\n Program finished by user !!!!")
     sys.exit()
